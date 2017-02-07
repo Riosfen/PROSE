80e9d9876ff2a4e12fb24086b5661f58fcb143c2
Riosfen <Riosfen@gmail.com>
2017-02-07 01:44:44 +0100

    java beans jpanel

---
+++ ProyectoChat/src/proyectochat/ProyectoChat.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectochat;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import proyectochat.vista.VistaPrincipal;
+
+/**
+ *
+ * @author samo_
+ */
+public class ProyectoChat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JFrame ventana = new JFrame("Servidor de mensajeria simple");
+        VistaPrincipal vista = new VistaPrincipal();
+        
+        ventana.add(vista);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(800, 500);
+        ventana.setVisible(true);
+        ventana.setMinimumSize(new Dimension(500, 200));
+        
+    }
+    
+}
