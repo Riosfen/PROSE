db8fe5c587d2132f04b1bcbabcacc95494c81860
alumno2damdiurno <samo_karmo@hotmail.es>
2017-02-24 10:18:48 +0100

    Interfaz grafica

---
+++ ProyectoFTP/src/proyectoftp/ProyectoFTP.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoftp;
+
+import proyectoftp.vista.VistaPrincipal;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author alumno2damdiurno
+ */
+public class ProyectoFTP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JFrame ventana = new JFrame("Cliente FTP");
+        VistaPrincipal vista = new VistaPrincipal();
+        
+    }
+    
+}
